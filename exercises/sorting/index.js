deabd11d17e6fa751ef2e235350d3d603cde579c
Jose Lima Neto <jose@basiscomply.com>
2019-01-30 18:14:39 -0300

    feat: selection by sort algorithm implementatio

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -11,10 +11,28 @@ function bubbleSort(arr) {
       }
     }
   }
+
   return arr;
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+
+  return arr;
+}
 
 function mergeSort(arr) {}
 
