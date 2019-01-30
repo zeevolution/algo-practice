64b4d09b3bed728b075b6bb6f1835c2e22130d32
Jose Lima Neto <jose@basiscomply.com>
2019-01-30 18:20:29 -0300

    feat: merge sort algorithm implementation

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -34,6 +34,30 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
 
-module.exports = { bubbleSort, selectionSort, mergeSort };
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const results = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
+}
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
