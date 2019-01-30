0793037117fa88089ca511ae77bb86dc0a6e9010
Jose Lima Neto <jose@basiscomply.com>
2019-01-30 09:58:48 -0300

    feat: init node's and linkedlist's classes

---
+++ exercises/linkedlists/index.js
@@ -0,0 +1,18 @@
+// -- Formulation
+// Implement classes Node and Linked Lists
+// See 'directions' document
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
+module.exports = { Node, LinkedList };
