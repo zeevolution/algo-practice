b046e351cdbc17a3184cd958c851aa9770063872
Jose Lima Neto <jose@basiscomply.com>
2019-01-30 12:36:41 -0300

    feat: check circular linked lists

---
+++ exercises/cicular/index.js
@@ -0,0 +1,31 @@
+// -- Problem's Formulation
+// Given a linked list, return true if the list
+// is circular, false if it is not.
+// -- e.g:
+//   const l = new List();
+//   const a = new Node('a');
+//   const b = new Node('b');
+//   const c = new Node('c');
+//   l.head = a;
+//   a.next = b;
+//   b.next = c;
+//   c.next = b;
+//   circular(l) // true
+
+function circular(list) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+module.exports = circular;
